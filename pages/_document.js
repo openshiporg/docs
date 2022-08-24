26810f151465f82348c3e3e2571aecbd0ab767f4
Junaid <junaidkabani3@gmail.com>
2022-08-23 23:46:37 -0500

    Update Umami script

--- pages/_document.js
+++ pages/_document.js
@@ -10,15 +10,14 @@ export default class _Document extends Document {
     return (
       <Html lang="en">
         <Head>
-          {typeof window !== "undefined" &&
-            process.env.NODE_ENV === "production" && (
-              <script
-                async
-                defer
-                data-website-id="3a416c79-2b14-4ed8-b148-994137e3f6a7"
-                src="https://stats.openship.org/umami.js"
-              />
-            )}
+          {typeof window !== "undefined" && (
+            <script
+              async
+              defer
+              data-website-id="3a416c79-2b14-4ed8-b148-994137e3f6a7"
+              src="https://stats.openship.org/umami.js"
+            />
+          )}
         </Head>
         <body>
           <Main />
